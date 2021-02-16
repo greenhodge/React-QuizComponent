55edc057bc04af0db9ed97114a4803bdc4f76b47
Aaron Hodge Silver <aaron@springthistle.com>
2021-02-16 16:17:02 -0600

    Module 7

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -5,16 +5,16 @@ class QuizQuestion extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      error: "",
+      incorrectAnswer: false,
     };
   }
 
   handleClick(buttonText) {
     if (buttonText === this.props.quiz_question.answer) {
-      this.setState({ error: "" });
+      this.setState({ incorrectAnswer: false });
       this.props.showNextQuestionHandler();
     } else {
-      this.setState({ error: buttonText + " is the wrong answer. Try again!" });
+      this.setState({ incorrectAnswer: true });
     }
   }
 
@@ -39,9 +39,7 @@ class QuizQuestion extends Component {
             )}
           </ul>
         </section>
-        <section>
-          <p>{this.state.error}</p>
-        </section>
+        { this.state.incorrectAnswer ? <p className="error">Sorry, that is not right</p> : null }
       </main>
     );
   }
